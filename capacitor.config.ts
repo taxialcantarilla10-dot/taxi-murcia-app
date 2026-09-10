c129cce3b48cdbec474a6f4c0d6668f232a2674c
taxialcantarilla10-dot <taxialcantarilla10@gmail.com>
2026-09-10 13:19:25 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,14 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'es.taximurcia.app',
   appName: 'Taxi Murcia',
-  webDir: '.',
+  webDir: 'www',
+
   server: {
     androidScheme: 'https'
   },
+
   plugins: {
     SplashScreen: {
       launchShowDuration: 0
     },
+
     Geolocation: {
       permissions: [
         'location',
@@ -18,6 +21,7 @@ const config: CapacitorConfig = {
       ]
     }
   },
+
   android: {
     backgroundColor: '#f5f8f7'
   }
